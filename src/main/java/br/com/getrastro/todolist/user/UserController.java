11b52d9c876afa43a6e70f5dfa3ca24dae1c8a2b
nelson2804 <nelsonarrais9@gmail.com>
2023-10-12 20:48:24 -0300

    password cliptography

--- src/main/java/br/com/getrastro/todolist/user/UserController.java
+++ src/main/java/br/com/getrastro/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -19,14 +21,19 @@ public class UserController {
     public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.UserRepository.findByUsername(userModel.getUsername());
 
-            if(user != null) {
-                System.out.println("Usuário já existe");
+        if (user != null) {
 
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
-            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var passwordHashred = BCrypt.withDefaults()
+                .hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
+
         var userCreated = this.UserRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
 
     }
-    
+
 }
