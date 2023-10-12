322284df47091cf9e43dab15e907a8cd586cfecb
nelson2804 <nelsonarrais9@gmail.com>
2023-10-12 19:56:26 -0300

    user validations

---
+++ src/main/java/br/com/getrastro/todolist/user/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.getrastro.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    
+
+    
+}
